65b5f9bc243bc819989ff9606033ef8017ca968d
RamiroJAP <joemarverueco78@gmail.com>
2026-03-28 22:48:31 +0800

    INFO
    -CHANGING INFO

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -83,7 +83,7 @@ export default function About() {
           <p>Have questions? We'd love to hear from you!</p>
           <div className="contact-info">
             <p><strong>Email Address:</strong> arevalo.paulina29@gmail.com</p>
-            <p><strong>Phone Number:</strong> +63 962 861 3540</p>
+            <p><strong>Phone Number:</strong>  0962 861 3540</p>
             <p><strong>Store Address:</strong> Sta Rita Public Market, Guiguinto, Bulacan</p>
             <p><strong>Opening Hours:</strong> Monday to Sunday: 7:00 AM - 6:00 PM </p>
           </div>
